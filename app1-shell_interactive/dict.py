c82a0f843963b6958ac89a848a4817419e3188a6
Anurag <anuragkumarak95@gmail.com>
2017-11-25 09:52:19 +0530

    initial commit with app1-shell-interactive application.

---
+++ app1-shell_interactive/dict.py
@@ -0,0 +1,31 @@
+from  difflib import get_close_matches
+import json
+
+data = json.load(open("data.json"))
+
+def means(word):
+    word = word.lower()
+    if word in data.keys():
+        return data[word]
+    elif word.title() in data.keys():
+        return data[word.title()]
+    else:
+        similars = get_close_matches(word, data.keys(),n=1, cutoff=0.8)
+        if len(similars)==1:
+            resp = input("Did you meant %s [Y/n]? :" % similars[0])
+            if resp.lower() == 'y':
+                return data[similars[0]]
+            else:
+                return "Word not found in dictionary!"
+        else:
+            return "Word not found in dictionary!"
+
+
+if __name__=="__main__":
+    word = input("Enter a Word :")
+    meaning = means(word)
+    if type(meaning) == list:
+        for idx, i in enumerate(meaning):
+            print (idx,i)
+    else:
+        print(meaning)
